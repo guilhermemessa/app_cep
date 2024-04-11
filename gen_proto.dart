8af5045a9400da5c9a3482820884c78b637debfb
Guilherme Messa <guilhermemessa@hotmail.com>
2024-04-11 17:48:31 -0300

    test gen exports proto

--- gen_proto.dart
+++ gen_proto.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() async {
-  final exportFilePath = './lib/src/dart/proto_exports.dart';
+  final exportFilePath = './lib/proto_exports.dart';
   final exportFile = await File(exportFilePath).create(recursive: true);
 
   final dir = Directory('./lib/src/dart/');
@@ -10,7 +10,7 @@ void main() async {
   String exportText = "";
 
   entities.forEach((element) {
-    exportText += "export \"${element.path.split("/").last}\"; \n";
+    exportText += "export \"./src/dart/${element.path.split("/").last}\"; \n";
   });
 
   exportFile.writeAsStringSync(exportText);
